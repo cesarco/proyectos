1e0abffe29b6773140aaee29a61af28edd1734f2
cesarco <rodriguezcesar_125@outlook.es>
2021-06-19 21:04:26 -0500

    hola

---
+++ universidadJava/practicas.java
@@ -0,0 +1,6 @@
+public class universidadJava {
+
+    public static void main(String args[]) { // main permite ejecutar el programa
+        System.out.println("Cesar Luis Rodriguez Solera");
+    }
+}
