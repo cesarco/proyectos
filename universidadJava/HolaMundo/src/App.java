584cc17b3d0aa878bb9a4f8e977aeae0a2fcb7d2
Cesar Roriguez <rodriguezcesar_125@outlook.es>
2021-06-28 11:34:46 -0500

    borrar archivos basura

--- universidadJava/HolaMundo/src/App.java
+++ universidadJava/HolaMundo/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World! my name is Cesar Luis 2.0 HOLA");
     }
 }
