592b8d21af1434797abe840d74ea2f35185ff1b4
Cesar Roriguez <rodriguezcesar_125@outlook.es>
2021-06-28 11:22:57 -0500

    Una version de el proyecto personal 2.0

--- universidadJava/HolaMundo/src/App.java
+++ universidadJava/HolaMundo/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! my name is Cesar Luis 2.0");
+        System.out.println("Hello, World! my name is Cesar Luis 2.0 HOLA");
     }
 }
